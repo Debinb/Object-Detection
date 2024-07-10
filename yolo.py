23d629cdc49f7c21fe45756416b21403242bf754
Debin Babykutty <96095584+Debinb@users.noreply.github.com>
2024-07-10 04:25:09 +0000

    Video implementation

--- yolo.py
+++ yolo.py
@@ -9,3 +9,51 @@ model = YOLO("yolov8m.pt")
 
 video_dir = '/workspaces/Object-Detection/'
 video_path = '/workspaces/Object-Detection/test.mp4' 
+
+if not os.path.exists(video_path):
+    print(f"Error: The file {video_path} does not exist.")
+else:
+    cap = cv2.VideoCapture(video_path)
+    print(f"VideoCapture object: {cap}")
+
+    # Check if the video was opened successfully
+    if not cap.isOpened():
+        print("Error: Could not open video file.")
+    else:
+        #While loop can go here to open video frame by frame. 
+        # Read a frame from the video
+        while True:  
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            results = model(frame, device="mps")
+            result = results[0]
+            boundingBoxes = np.array(result.boxes.xyxy.cpu(),dtype="int")
+            classes = np.array(result.boxes.cls.cpu(), dtype="int")
+            for cls, bbox in zip(classes, boundingBoxes):
+                (x, y, x2, y2) = bbox
+
+                 
+                cv2.rectangle(frame, (x, y), (x2, y2),(0, 0, 225), 2)
+                cv2.putText(frame, str(cls),(x, y -5), cv2.FONT_HERSHEY_PLAIN, 1,(225),2)
+            #print(boundingBoxes)
+            #print(results)
+            # Display the frame in a window named "Img"
+            cv2.imshow("Img", frame)
+            # Wait for a key press to close the window
+            key = cv2.waitKey(1)
+            # Destroy the window
+            #cv2.destroyAllWindows()
+           # else:
+                #break
+                #print("Error: Could not read frame from video.")
+
+            if key == 27:
+                break
+
+
+    # Release the video capture object
+    cap.release()
+    cv2.destroyAllWindows()
