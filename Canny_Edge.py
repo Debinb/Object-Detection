6c80d4232dde7ca7953e27d4adcbd01f974fca1d
Debin Babykutty <96095584+Debinb@users.noreply.github.com>
2024-07-23 18:56:16 +0000

    function implementation

--- Canny_Edge.py
+++ Canny_Edge.py
@@ -3,7 +3,19 @@ import numpy as np
 import torch
 from ultralytics import YOLO
 import os
-   
+
+def canny_edge_detection(frame): 
+    # Convert the frame to grayscale for edge detection 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+      
+    # Apply Gaussian blur to reduce noise and smoothen edges 
+    blurred = cv2.GaussianBlur(src=gray, ksize=(3, 5), sigmaX=0.5) 
+      
+    # Perform Canny edge detection 
+    edges = cv2.Canny(blurred, 70, 135) 
+      
+    return blurred, edges
+
 def main(): 
     # Open the default webcam  
     cap = cv2.VideoCapture(0) 
